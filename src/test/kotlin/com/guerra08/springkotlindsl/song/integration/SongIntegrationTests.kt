b47eefdd58c99bbbb3809a426ea83c00a77d4fab
Bruno Guerra <gguerrabruno@outlook.com>
2022-08-27 22:42:06 -0300

    More test cases

--- src/test/kotlin/com/guerra08/springkotlindsl/song/integration/SongIntegrationTests.kt
+++ src/test/kotlin/com/guerra08/springkotlindsl/song/integration/SongIntegrationTests.kt
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
-import org.springframework.test.web.servlet.post
-import org.springframework.test.web.servlet.put
+import org.springframework.test.web.servlet.*
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -41,7 +38,11 @@ class SongIntegrationTests(
     @Test
     fun getById_shouldReturnOkResponseIfSongExists() {
 
-        every { songService.getById(any()) } returns SongContract(name = "Hurt", album = "Downward Spiral", artist = "NIN")
+        every { songService.getById(any()) } returns SongContract(
+            name = "Hurt",
+            album = "Downward Spiral",
+            artist = "NIN"
+        )
 
 
         client.get("/song/{id}", 1L)
@@ -97,6 +98,48 @@ class SongIntegrationTests(
         }.andExpect {
             status { isOk() }
         }
+
+    }
+
+    @Test
+    fun putById_shouldReturnNotFoundIfSongDoesNotExists() {
+
+        val song = SongContract(name = "Hurt", album = "Downward Spiral", artist = "NIN")
+
+        every { songService.putById(any(), any()) } returns null
+
+        val payload = Gson().toJson(song)
+
+        client.put("/song/1") {
+            content = payload
+            contentType = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isNotFound() }
+        }
+
+    }
+
+    @Test
+    fun deleteById_shouldReturnNoContentIfDeleteIsSuccessful() {
+
+        every { songService.deleteById(any()) } returns true
+
+        client.delete("/song/1")
+            .andExpect {
+                status { isNoContent() }
+            }
+
+    }
+
+    @Test
+    fun deleteById_shouldReturnNotFound() {
+
+        every { songService.deleteById(any()) } returns false
+
+        client.delete("/song/1")
+            .andExpect {
+                status { isNotFound() }
+            }
 
     }
 
